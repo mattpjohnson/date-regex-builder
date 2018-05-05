60da7e00512f4be4a81b966d87ef709681da41c2
Matt Johnson <hello@mattpjohnson.com>
2018-05-05 14:19:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Date Regex Builder
+> Easily build date regular expressions
+
+![Coveralls github](https://img.shields.io/coveralls/github/mattpjohnson/date-regex-builder.svg)
+![npm](https://img.shields.io/npm/v/date-regex-builder.svg)
+![npm](https://img.shields.io/npm/dm/localeval.svg)
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+![license](https://img.shields.io/github/license/mattpjohnson/date-regex-builder.svg)
+
+# Installation
+> npm install date-regex-builder
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+const TokenToRegexLookup = {
+    YY: "\\d{2}",
+    YYYY: "\\d{4}",
+    M: "([1-9]|1[0-2])",
+    MM: "(0[1-9]|1[0-2])",
+    MMM: "(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)",
+    MMMM:
+        "(January|February|March|April|May|June|July|August|September|October|November|December)",
+    D: "([1-9]|[1-2][0-9]|3[0-1])",
+    DD: "(0[1-9]|[1-2][0-9]|3[0-1])",
+};
+
+type SupportedLocale = "en" | "es";
+
+const drb = {
+    defaultConfig: {
+        // tokenizer: tokenizer([[0, 2], [2, 3], [3, 5], [5, 6], [6, 10]])
+    },
+    pure(str: string, { formatter, tokenizer }) {
+        return drb.tokens(tokenizer(str), { formatter });
+    },
+    tokens(tokens: Array<string>, { formatter }) {
+        return tokens.map(token => formatter(token)).join("");
+    },
+    offsetTokenizer(offsets: Array<[number, number]>) {
+        return string =>
+            offsets.map(([start, finish]) => string.slice(start, finish));
+    },
+    formatter(locale: SupportedLocale) {
+        return function(token: string) {
+            if (token in TokenToRegexLookup) {
+                return TokenToRegexLookup[token];
+            }
+
+            return token;
+        };
+    },
+};
+
+const result = drb.pure("MMM D, YYYY", {
+    formatter: drb.formatter("en"),
+    tokenizer: drb.offsetTokenizer([[0, 3], [3, 4], [4, 5], [5, 7], [7, 11]]),
+});
+
+export { drb as DateRegexBuilder };
